f181296194678b154267f6b35c013d7d217695f3
PLimon48 <55422643+PLimon48@users.noreply.github.com>
2019-11-07 11:26:07 -0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+$('.hinge-click').click(function(e) {
+      $(this).addClass('animated hinge');
+})
+
+$('.hinge-click').click(function(e) {
+  console.log("The FBI has locked onto your location, do not try to flee.");
+})
